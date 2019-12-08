dc12954909cfca6a2ebfc83f1fdabe50f1adc508
Holmes7 <ashutoshpatkar@yahoo.in>
2019-12-08 17:27:50 +0530

    created a new file maps.py to keep the game map

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # Set basic constant colour RGB codes
-BLUE = (255,0,0)
-GREEN = (0,255,0)
-RED = (0,0,255)
-BLACK = (0,0,0)
-WHITE = (255,255,255)
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # Set basic variables
 HEIGHT = 577
